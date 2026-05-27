18b9a7251f6d1ab9c29111a1301059cae42d7778
lc <1641767724@qq.com>
2026-05-27 22:56:10 +0800

    完善rtsp

--- study/main.cpp
+++ study/main.cpp
@@ -12,11 +12,13 @@
 #include <string>
 #pragma comment(lib, "ws2_32.lib")
 #include <stdint.h>
+#pragma warning( disable : 4996 )
 
 #define SERVER_PORT      8554
 
 #define SERVER_RTP_PORT  55532
 #define SERVER_RTCP_PORT 55533
+
 
 static int createSocket() {
 	int sockfd = 0;
@@ -62,29 +64,66 @@ static int acceptClient(int sockfd,char *ip,int *port) {
 
 static int handleCmd_OPTIONS(char* result, int cseq)
 {
- 
+	sprintf(result, "RTSP/1.0 200 OK\r\n"
+		"CSeq:%d\r\n"
+		"Public: OPTIONS, DESCRIBE, SETUP, PLAY\r\n"
+		"\r\n",
+		cseq);
     return 0;
 }
 
 static int handleCmd_DESCRIBE(char* result, int cseq, char* url)
 {
+	char sdp[500];
+	char localIp[100];
+
+	sscanf(url, "rtsp://%[^:]:", localIp);
+
+	sprintf(sdp,"v=0\r\n"
+        "o=- 9%ld 1 IN IP4 %s\r\n"
+        "t=0 0\r\n"
+        "a=control:*\r\n"
+        "m=video 0 RTP/AVP 96\r\n"
+        "a=rtpmap:96 H264/90000\r\n"
+		"a=control:track0\r\n",
+		time(NULL), localIp);
+
+	sprintf(result, "RTSP/1.0 200 OK\r\nCSeq: %d\r\n"
+		"Content-Base: %s\r\n"
+		"Content-type: application/sdp\r\n"
+		"Content-length: %zu\r\n\r\n"
+		"%s",
+		cseq,
+		url,
+		strlen(sdp),
+		sdp);
 
     return 0;
 }
 
 static int handleCmd_SETUP(char* result, int cseq, int clientRtpPort)
 {
+	sprintf(result, "RTSP/1.0 200 OK\r\n"
+		"CSeq: %d\r\n"
+		"Transport: RTP/AVP;unicast;client_port=%d-%d;server_port=%d-%d\r\n"
+		"Session: 66334873\r\n"
+		"\r\n",
+		cseq,
+		clientRtpPort,
+		clientRtpPort + 1,
+		SERVER_RTP_PORT,
+		SERVER_RTCP_PORT);
    
     return 0;
 }
 
 static int handleCmd_PLAY(char* result, int cseq)
 {
-  /*  sprintf(result, "RTSP/1.0 200 OK\r\n"
-        "CSeq: %d\r\n"
-        "Range: npt=0.000-\r\n"
-        "Session: 66334873; timeout=10\r\n\r\n",
-        cseq);*/
+    sprintf(result, "rtsp/1.0 200 ok\r\n"
+        "cseq: %d\r\n"
+        "range: npt=0.000-\r\n"
+        "session: 66334873; timeout=10\r\n\r\n",
+        cseq);
 
     return 0;
 }
